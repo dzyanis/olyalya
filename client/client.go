9f984fc45e707c902d575b9ff62b0b34aca0d8c2
Dzyanis Kuzmenka <dzyanis@gmail.com>
2017-02-13 01:50:36 +0300

    added client library

--- client/client.go
+++ client/client.go
@@ -23,18 +23,36 @@ func NewClient(addr string, port int) *Client {
 
 // curl -X POST -H 'Content-Type: application/json' 'http://localhost:8080/db/create' -d '{"name": "dz"}'
 func (c *Client) Create(dbName string) error {
-	body, err := json.Marshal(map[string]string{
+	_, err := c.conn.Post("/db/create", map[string]interface{}{
 		"name": dbName,
 	})
 	if err != nil {
 		return err
 	}
-	_, err = c.conn.methodPost("/db/create", body)
-	if err != nil {
-		return err
-	}
 	c.dbName = dbName
 	return nil
+}
+
+func (c *Client) DbList() ([]string, error) {
+	req, err := c.conn.Get("/db/list", map[string]interface{}{})
+	if err != nil {
+		return []string{}, err
+	}
+	defer req.Body.Close()
+
+	type JsonList struct {
+		Count int 	`json:"count"`
+		Names []string	`json:"names"`
+	}
+	var jl JsonList
+
+	decoder := json.NewDecoder(req.Body)
+	err = decoder.Decode(&jl)
+	if err != nil {
+		return []string{}, err
+	}
+
+	return jl.Names, nil
 }
 
 func (c *Client) Select(dbName string) error {
@@ -48,14 +66,10 @@ func (c *Client) Set(name string, value string) error {
 		return ErrClientDbWasNotSelected
 	}
 
-	body, err := json.Marshal(map[string]string{
+	_, err := c.conn.Post(fmt.Sprintf("/db/%s/set", c.dbName), map[string]interface{}{
 		"name": name,
 		"value": value,
 	})
-	if err != nil {
-		return err
-	}
-	_, err = c.conn.methodPost(fmt.Sprintf("/db/%s/set", c.dbName), body)
 	if err != nil {
 		return err
 	}
@@ -64,18 +78,13 @@ func (c *Client) Set(name string, value string) error {
 
 // curl -X GET 'http://localhost:8080/db/dz/get/one'
 func (c *Client) Get(name string) (string, error) {
-	if c.dbName == "" {
-		return "", ErrClientDbWasNotSelected
-	}
-
-	body := []byte{}
-	r, err := c.conn.methodGet(fmt.Sprintf("/db/%s/get/%s", c.dbName, name), body)
-	if err != nil {
-		return "", err
-	}
-	b, err := c.conn.ReadResponse(r)
-	if err != nil {
-		return b, err
-	}
-	return b, nil
+	//r, err := c.conn.Get(fmt.Sprintf("/db/%s/get/%s", c.dbName, name), map[string]interface{}{})
+	//if err != nil {
+	//	return "", err
+	//}
+	//b, err := c.conn.ReadResponse(r)
+	//if err != nil {
+	//	return b, err
+	//}
+	//return b, nil
 }
--- client/connection.go
+++ client/connection.go
@@ -2,63 +2,67 @@ package client
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"bytes"
 	"bufio"
+	"time"
+	"encoding/json"
 )
 
 type Connection struct {
 	Addr string
 	Port int
+	Http *http.Client
 }
 
 func NewConnection(addr string, port int) *Connection {
+	var netTransport = &http.Transport{
+		Dial: (&net.Dialer{Timeout: 5 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+	}
+	var http = &http.Client{
+		Timeout: time.Second * 10,
+		Transport: netTransport,
+	}
 	return &Connection{
 		Addr: addr,
 		Port: port,
+		Http: http,
 	}
 }
 
-func (c *Connection) GetFullURL(uri string) string {
+func (c *Connection) Url(uri string) string {
 	return fmt.Sprintf("http://%s:%d%s", c.Addr, c.Port, uri)
 }
 
-func (c *Connection) request(method string, uri string, body []byte) (*http.Response, error) {
-	url := c.GetFullURL(uri)
+func (c *Connection) request(method string, url string, data map[string]interface{}) (*http.Response, error) {
+	body, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
 	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return resp, err
-	}
-	defer resp.Body.Close()
-	return resp, nil
+	return c.Http.Do(req)
 }
 
-func (c *Connection) methodGet(uri string, body []byte) (*http.Response, error)  {
-	return c.request("GET", uri, body)
+func (c *Connection) Put(uri string, data map[string]interface{}) (*http.Response, error)  {
+	return c.request("PUT", c.Url(uri), data)
 }
 
-func (c *Connection) methodPost(uri string, body []byte) (*http.Response, error) {
-	return c.request("POST", uri, body)
+func (c *Connection) Get(uri string, data map[string]interface{}) (*http.Response, error)  {
+	return c.request("GET", c.Url(uri), data)
 }
 
-func (c *Connection) methodDelete(uri string, body []byte) (*http.Response, error)  {
-	return c.request("DELETE", uri, body)
+func (c *Connection) Post(uri string, data map[string]interface{}) (*http.Response, error) {
+	return c.request("POST", c.Url(uri), data)
 }
 
-func (c *Connection) ReadResponse(response *http.Response) (string, error)  {
-	scanner := bufio.NewScanner(response.Body)
-	scanner.Split(bufio.ScanRunes)
-	var buf bytes.Buffer
-	for scanner.Scan() {
-		return scanner.Text(), nil;
-		buf.WriteString(scanner.Text())
-	}
-	return buf.String(), nil
+func (c *Connection) Delete(uri string, data map[string]interface{}) (*http.Response, error)  {
+	return c.request("DELETE", c.Url(uri), data)
 }

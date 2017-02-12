b8a260171ccbe5f6184eba114b2e757c8f734a16
Dzyanis Kuzmenka <dzyanis@gmail.com>
2017-02-12 18:01:17 +0300

    added lexer

---
+++ cmd/lexer.go
@@ -0,0 +1,176 @@
+// @todo Add Number type
+// @todo Fixed a problem \\" for strings
+package cmd
+
+import(
+	"errors"
+	"regexp"
+)
+
+var (
+	ErrTermIsWrong = errors.New("Term is wrong")
+)
+
+const (
+	STATUS_NONE  = iota
+	STATUS_OPEN  = iota
+	STATUS_CLOSE = iota
+)
+
+const (
+	TERM_NONE   = iota
+	TERM_ARRAY  = iota
+	TERM_HASH   = iota
+	TERM_STRING = iota
+	TERM_NAME   = iota
+)
+
+type Lexer struct {
+	typeTerm int
+	status int
+	curTerm string
+	terms []string
+}
+
+func NewLexer() *Lexer {
+	return &Lexer{}
+}
+
+func (lx *Lexer) termAdd(str string) {
+	lx.curTerm = lx.curTerm + str
+}
+
+func (lx *Lexer) termSet(str string) {
+	lx.curTerm = str
+}
+
+func (lx *Lexer) IsStatusOpen() bool {
+	return lx.status == STATUS_OPEN
+}
+
+func (lx *Lexer) IsArrayBegin(ch byte) bool {
+	return ch == '['
+}
+
+func (lx *Lexer) IsArrayEnd(ch byte) bool {
+	return ch == ']'
+}
+
+func (lx *Lexer) IsHashBegin(ch byte) bool {
+	return ch == '{'
+}
+
+func (lx *Lexer) IsHashEnd(ch byte) bool {
+	return ch == '}'
+}
+
+func (lx *Lexer) IsStringBegin(ch byte) bool {
+	return ch == '"'
+}
+
+func (lx *Lexer) IsStringEnd(ch byte) bool {
+	return ch == '"'
+}
+
+func (lx *Lexer) TermType() int {
+	return lx.typeTerm
+}
+
+func (lx *Lexer) IsPreviusCharEscape() bool {
+	ln := len(lx.curTerm)
+	if ln > 1 {
+		return lx.curTerm[ln-1] == '\\'
+	}
+	return false
+}
+
+func (lx *Lexer) Open(termType int) {
+	lx.status = STATUS_OPEN
+	lx.typeTerm = termType
+}
+
+func (lx *Lexer) Close() {
+	lx.status = STATUS_NONE
+	lx.typeTerm = TERM_NONE
+	lx.terms = append(lx.terms, lx.curTerm)
+	lx.curTerm = ""
+}
+
+func (lx *Lexer) IsSpace(b byte) bool {
+	r := regexp.MustCompile(`[ \t\r\n\f]`)
+	return r.Match([]byte{b})
+}
+
+func (lx *Lexer) Parse(cmd string) error {
+	for _, symbol := range cmd {
+		err := lx.Step(byte(symbol))
+		if err!=nil {
+			return err
+		}
+	}
+
+	if lx.IsStatusOpen() {
+		lx.Close()
+	}
+
+	return nil
+}
+
+func (lx *Lexer) Step(ch byte) error {
+	if !lx.IsStatusOpen() {
+		if lx.IsArrayBegin(ch) {
+			lx.Open(TERM_ARRAY)
+			lx.termSet(`[`)
+		} else if lx.IsHashBegin(ch) {
+			lx.Open(TERM_HASH)
+			lx.termSet(`{`)
+		} else if lx.IsStringBegin(ch) {
+			lx.Open(TERM_STRING)
+			lx.termSet(`"`)
+		} else if !lx.IsSpace(ch) {
+			lx.Open(TERM_NAME)
+			lx.termSet(string(ch))
+		} else {
+			//return ErrTermIsWrong
+		}
+		return nil
+	}
+
+	if lx.IsStatusOpen() {
+		switch lx.TermType() {
+		case TERM_ARRAY:
+			if lx.IsArrayEnd(ch) {
+				lx.termAdd(`]`)
+				lx.Close()
+			} else {
+				lx.termAdd(string(ch))
+			}
+		case TERM_HASH:
+			if lx.IsHashEnd(ch) {
+				lx.termAdd(`}`)
+				lx.Close()
+			} else {
+				lx.termAdd(string(ch))
+			}
+		case TERM_STRING:
+			if lx.IsStringEnd(ch) && !lx.IsPreviusCharEscape() {
+				lx.termAdd(`"`)
+				lx.Close()
+			} else {
+				lx.termAdd(string(ch))
+			}
+		case TERM_NAME:
+			if lx.IsSpace(ch) {
+				lx.Close()
+			} else {
+				lx.termAdd(string(ch))
+			}
+		}
+	}
+
+	return nil
+}
+
+func (lx *Lexer) GetTerms() []string {
+	return lx.terms
+}
---
+++ cmd/lexer_test.go
@@ -0,0 +1,176 @@
+package cmd
+
+import (
+	"testing"
+)
+
+var (
+	lex = NewLexer()
+)
+
+func TestIsArrayBegin(t *testing.T) {
+	if !lex.IsArrayBegin('[') {
+		t.Error("Unexpected behavior")
+	}
+	if lex.IsArrayBegin('a') {
+		t.Error("Unexpected behavior")
+	}
+}
+
+func TestIsArrayEnd(t *testing.T) {
+	if !lex.IsArrayEnd(']') {
+		t.Error("Unexpected behavior")
+	}
+	if lex.IsArrayEnd('a') {
+		t.Error("Unexpected behavior")
+	}
+}
+
+func TestIsHashBegin(t *testing.T) {
+	if !lex.IsHashBegin('{') {
+		t.Error("Unexpected behavior")
+	}
+	if lex.IsHashBegin('a') {
+		t.Error("Unexpected behavior")
+	}
+}
+
+func TestIsHashEnd(t *testing.T) {
+	if !lex.IsHashEnd('}') {
+		t.Error("Unexpected behavior")
+	}
+	if lex.IsHashEnd('a') {
+		t.Error("Unexpected behavior")
+	}
+}
+
+func TestIsStringBegin(t *testing.T) {
+	if !lex.IsStringBegin('"') {
+		t.Error("Unexpected behavior")
+	}
+	if lex.IsStringBegin('a') {
+		t.Error("Unexpected behavior")
+	}
+}
+
+func TestIsStringEnd(t *testing.T) {
+	if !lex.IsStringEnd('"') {
+		t.Error("Unexpected behavior")
+	}
+	if lex.IsStringEnd('a') {
+		t.Error("Unexpected behavior")
+	}
+}
+
+func TestIsSpace(t *testing.T) {
+	if !lex.IsSpace(' ') {
+		t.Error("Unexpected behavior")
+	}
+
+	if !lex.IsSpace('\t') {
+		t.Error("Unexpected behavior")
+	}
+
+	if !lex.IsSpace('\n') {
+		t.Error("Unexpected behavior")
+	}
+
+	if lex.IsSpace('a') {
+		t.Error("Unexpected behavior")
+	}
+}
+
+func TestIsPreviusCharEscape(t *testing.T) {
+	lex.termSet("")
+	if lex.IsPreviusCharEscape() {
+		t.Error("Unexpected behavior")
+	}
+
+	lex.termSet("TEST")
+	if lex.IsPreviusCharEscape() {
+		t.Error("Unexpected behavior")
+	}
+
+	lex.termAdd("\\")
+	if !lex.IsPreviusCharEscape() {
+		t.Error("Unexpected behavior")
+	}
+}
+
+func TestIsStatusOpen(t *testing.T) {
+	lex.Close()
+	if lex.IsStatusOpen() {
+		t.Error("Unexpected behavior")
+	}
+
+	lex.Open(TERM_NAME)
+	if !lex.IsStatusOpen() {
+		t.Error("Unexpected behavior")
+	}
+
+	lex.Close()
+	if lex.IsStatusOpen() {
+		t.Error("Unexpected behavior")
+	}
+}
+
+func TestClose(t *testing.T) {
+	ls := NewLexer()
+	ls.termSet("TEST1")
+	ls.Close()
+	ls.Close()
+	ls.termSet("TEST2")
+	ls.Close()
+	terms := ls.GetTerms()
+	if terms[0] != "TEST1" {
+		t.Error("Unexpected behavior")
+	}
+	if terms[1] != "" {
+		t.Error("Unexpected behavior")
+	}
+	if terms[2] != "TEST2" {
+		t.Error("Unexpected behavior")
+	}
+}
+
+func TestParseCommandName(t *testing.T) {
+	ls := NewLexer()
+	err := ls.Parse(` SET/INDEX/ARRAY value `)
+	if err!=nil {
+		t.Error(err)
+	}
+	if ls.GetTerms()[0] != "SET/INDEX/ARRAY" {
+		t.Error("Unexpected behavior")
+	}
+}
+
+func TestParse(t *testing.T) {
+	ls := NewLexer()
+	err := ls.Parse(`   TEST hello " World!" 42 ["1", "2", "3"] {"zero":"0", "two":"1"}   `)
+	if err!=nil {
+		t.Error(err)
+	}
+
+	terms := ls.GetTerms()
+	if len(terms) < 6 {
+		t.Error("Unexpected behavior")
+	}
+	if terms[0] != "TEST" {
+		t.Error("Unexpected behavior")
+	}
+	if terms[1] != "hello" {
+		t.Error("Unexpected behavior")
+	}
+	if terms[2] != "\" World!\"" {
+		t.Error("Unexpected behavior")
+	}
+	if terms[3] != "42" {
+		t.Error("Unexpected behavior")
+	}
+	if terms[4] != "[\"1\", \"2\", \"3\"]" {
+		t.Error("Unexpected behavior")
+	}
+	if terms[5] != "{\"zero\":\"0\", \"two\":\"1\"}" {
+		t.Error("Unexpected behavior")
+	}
+}
